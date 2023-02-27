56a9872dcb7ef1bade17398ced5ea6f6436187fb
Mathias Klenk <mathiasklenk@gmx.de>
2023-02-27 15:04:35 +0100

    Setup cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ObservedReposModule } from './observed-repos/observed-repos.module';
+import { CronService } from './cron/cron.service';
 
 @Module({
-  imports: [ObservedReposModule],
+  imports: [ObservedReposModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronService],
 })
 export class AppModule {}
